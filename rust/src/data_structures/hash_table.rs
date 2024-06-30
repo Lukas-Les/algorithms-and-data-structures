b530e44cf41ff1c507367697440e0a532ffb4527
stabmeldys <lukas.les93@gmail.com>
2024-06-30 17:02:14 +0300

    mod hashing

--- rust/src/data_structures/hash_table.rs
+++ rust/src/data_structures/hash_table.rs
@@ -1,25 +1,49 @@
-const FNV_OFFSET_BASIS: u64 = 0xcbf29ce484222325;
-const FNV_PRIME: u64 = 0x100000001b3;
+pub mod hashing {
+    const FNV_OFFSET_BASIS: u64 = 0xcbf29ce484222325;
+    const FNV_PRIME: u64 = 0x100000001b3;
 
-
-fn fnv1a_hash(s: &str) -> u64 {
-    let mut hash = FNV_OFFSET_BASIS;
-    for byte in s.as_bytes() {
-        hash ^= u64::from(*byte);
-        hash = hash.wrapping_mul(FNV_PRIME);
+    fn fnv1a_hash(bytes: &[u8]) -> u64 {
+        let mut hash = FNV_OFFSET_BASIS;
+        for &byte in bytes {
+            hash ^= u64::from(byte);
+            hash = hash.wrapping_mul(FNV_PRIME);
+        }
+        hash
     }
-    hash
+
+
+    pub trait Hashable {
+        fn hash(&self) -> u64;
+    }
+
+    impl Hashable for &str {
+        fn hash(&self) -> u64 {
+            fnv1a_hash(self.as_bytes())
+        }
+    }
+
+    impl Hashable for i32 {
+        fn hash(&self) -> u64 {
+            fnv1a_hash(&self.to_le_bytes())
+        }
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::hashing::Hashable;
 
     #[test]
-    fn test_fnv1a_hash() {
-        let some_str = "hello";
-        let hash_sum = fnv1a_hash(some_str);
+    fn test_fnv1a_hash_str() {
+        let hash_sum = "hello".hash();
         assert_eq!(hash_sum, 11831194018420276491);
+    }
+
+    #[test]
+    fn test_fnv1a_hash_i32() {
+        let n: i32 = 69;
+        let hash_sum = n.hash();
+        assert_eq!(hash_sum, 3164654411128539488);
     }
 }
