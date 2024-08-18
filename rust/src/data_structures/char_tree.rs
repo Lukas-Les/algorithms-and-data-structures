ef30cc89174c03b922349389f9b72988616258d1
stabmeldys <lukas.les93@gmail.com>
2024-08-18 20:09:41 +0300

    char tree get

--- rust/src/data_structures/char_tree.rs
+++ rust/src/data_structures/char_tree.rs
@@ -15,6 +15,10 @@ impl Node {
         }
     }
 
+    fn get_child(&self, name: char) -> Option<&Box<Node>> {
+        self.children.iter().find(|node| node.name == name)
+    }
+
     fn get_child_mut(&mut self, name: char) -> Option<&mut Box<Node>> {
         self.children.iter_mut().find(|node| node.name == name)
     }
@@ -46,7 +50,7 @@ impl Tree {
         }
     }
 
-    fn insert(&mut self, mut path: &str, value: &str) {
+    pub fn insert(&mut self, mut path: &str, value: &str) {
         if self.root.is_none() {
             let first_char = path.chars().next().unwrap();
             path = &path[1..];
@@ -56,6 +60,22 @@ impl Tree {
         
         let mut current_node = self.root.as_mut().unwrap();
         Self::insert_recursive(path, value, current_node);
+    }
+
+    fn get(&self, mut path: &str) -> Option<String> {
+        if self.root.is_none() {
+            return None;
+        }
+        let mut current_node = self.root.as_ref().unwrap();
+        while !path.is_empty() {
+            let first_char = path.chars().next().unwrap();
+            path = &path[1..];
+            match current_node.get_child(first_char) {
+                Some(child) => current_node = child,
+                None => break,
+            }
+        }
+        current_node.value.clone()
     }
     
 }
@@ -68,5 +88,11 @@ mod tests {
         let mut node = Node::new('a');
         node.children.push(Box::new(Node::new('b')));
         assert_eq!(node.get_child_mut('b').unwrap().name, 'b');
+    }
+    #[test]
+    fn test_tree() {
+        let mut tree = Tree::new();
+        tree.insert("abc", "ABC");
+        assert_eq!(tree.get("abc").unwrap(), "ABC".to_string());
     }
 }
