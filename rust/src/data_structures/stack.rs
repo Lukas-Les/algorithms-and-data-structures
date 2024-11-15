5bc8eed2289b82bfbb2daed6abfa6f8afb3f7ed3
stabmeldys <lukas.les93@gmail.com>
2024-11-15 05:39:50 +0200

    stack file to modules

--- rust/src/data_structures/stack.rs
+++ rust/src/data_structures/stack.rs
@@ -1,70 +1,70 @@
-#[derive(Debug)]
-struct Stack <const N: usize> {
-    items: [u8; N], 
-    top: usize,
+mod oo {
+    #[derive(Debug)]
+    pub struct Stack<const N: usize> {
+        items: [u8; N],
+        top: usize,
+    }
+
+    impl<const N: usize> Stack<N> {
+        pub const fn new() -> Self {
+            Self {
+                items: [0; N],
+                top: 0,
+            }
+        }
+
+        pub fn push(&mut self, value: u8) -> Result<(), &str> {
+            if self.top + 1 > self.items.len() {
+                return Err("Stack overflow");
+            }
+            self.items[self.top] = value;
+            self.top += 1;
+            Ok(())
+        }
+
+        pub fn pop(&mut self) -> Option<u8> {
+            if self.top < 1 {
+                return None;
+            }
+            let top_item_idx = self.top - 1;
+            let top_item: u8 = self.items[top_item_idx];
+            self.items[top_item_idx] = 0;
+            self.top -= 1;
+            Some(top_item)
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.top == 0
+        }
+
+        pub fn is_full(&self) -> bool {
+            self.top == self.items.len()
+        }
+
+        pub fn peek(&self) -> Option<u8> {
+            if self.top == 0 {
+                return None;
+            }
+            return Some(self.items[self.top - 1]);
+        }
+    }
 }
-
-impl <const N: usize> Stack <N> {
-    pub const fn new() -> Self {
-        Self {
-            items: [0; N],
-            top: 0,
-        }
-    }
-
-    pub fn push(&mut self, value: u8) -> Result<(), &str> {
-        if self.top + 1 > self.items.len() {
-            return Err("Stack overflow");
-        }
-        self.items[self.top] = value;
-        self.top += 1;
-        Ok(())
-    }
-
-    pub fn pop(&mut self) -> Option<u8> {
-        if self.top < 1 {
-            return None;
-        }
-        let top_item_idx = self.top - 1;
-        let top_item: u8 = self.items[top_item_idx];
-        self.items[top_item_idx] = 0;
-        self.top -= 1;
-        Some(top_item)
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.top == 0        
-    }
-    
-    pub fn is_full(&self) -> bool {
-        self.top == self.items.len()
-    }
-
-    pub fn peek(&self) -> Option<u8> {
-        if self.top == 0 {
-            return None;
-        }
-        return Some(self.items[self.top + 1]);
-    }
-}
-
 
 mod tests {
-    use super::Stack;
-    
+    use super::oo::Stack;
+
     fn prepare_stack() -> Stack<3> {
         let mut stack = Stack::new();
         (0..3).for_each(|i| {
             stack.push(i).expect("failed to push to test stack");
-            }
-        );
+        });
         stack
     }
 
     #[test]
     fn test_push() {
         let mut stack = prepare_stack();
-        assert_eq!(stack.items[0], 0);
+        assert_eq!(stack.peek(), Some(2));
 
         match stack.push(7) {
             Ok(()) => panic!("Expected an overflow"),
@@ -100,7 +100,6 @@ mod tests {
     #[test]
     fn test_peek() {
         let stack = prepare_stack();
-        assert_eq!(stack.peek(), Some(3));
+        assert_eq!(stack.peek(), Some(2));
     }
-
 }
