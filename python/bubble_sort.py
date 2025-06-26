18f25da14ffed831ba88be9ade2ad5067dd8a822
stabmeldys <lukas.les93@gmail.com>
2025-06-26 08:49:48 +0300

    updated bubble sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,21 +1,24 @@
-def buble_sort(input_list: list) -> list:
+def bubble_sort(input_list: list) -> list:
+    """
+    Sorts a list of elements using the Bubble Sort algorithm.
+
+    Args:
+        input_list: The list to be sorted.
+
+    Returns:
+        The sorted list.
+    """
     length = len(input_list)
-    i_loop = 0
-    j_loop = 0
     for i in range(length):
-        swaped = False
+        swapped = False
         for j in range(length - i - 1):
             if input_list[j] > input_list[j + 1]:
                 input_list[j + 1], input_list[j] = input_list[j], input_list[j + 1]
-                swaped = True
-            j_loop += 1
-        if not swaped:
+                swapped = True
+        if not swapped:
             break
-        i_loop += 1
-    print(f"i_loop: {i_loop}")
-    print(f"j_loop: {j_loop}")
     return input_list
 
 if __name__ == "__main__":
-    r = buble_sort([5, 1, 9, 2, 8, 7, 3, 4, 6, 0])
+    r = bubble_sort([5, 1, 9, 2, 8, 7, 3, 4, 6, 0])
     print(r)
